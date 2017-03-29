2524a25377a683a38bae4771961f4c9c9fed763b
Michael Woolweaver <mwoolweaver@users.noreply.github.com>
2017-03-29 01:35:01 -0500

    Update package.sh

--- package.sh
+++ package.sh
@@ -2,6 +2,6 @@ echo Creating New .deb’s
 
 # list all packages you want to create with the follow format 
 
-# dpkg-deb -Zlzma -b raw/[packe-name] deb/
+# dpkg-deb -Zlzma -b raw/[package-name] deb/
 
 dpkg-deb -Zlzma -b raw/Example_Anemone_Font deb/
